2ab7fa933653a1b9c396cb5da02826cb9cdcaa23
Princess <pcjbautista@gmail.com>
2020-11-11 01:29:02 -0800

    second page date picker

--- app/src/main/java/com/example/cc17uielements/NextActivity.kt
+++ app/src/main/java/com/example/cc17uielements/NextActivity.kt
@@ -1,37 +1,44 @@
 package com.example.cc17uielements
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.CalendarView
+import android.widget.DatePicker
+import android.widget.DatePicker.*
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_next.*
+import java.util.*
 
 class NextActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_next)
-        val fn = getIntent().getStringExtra("firstName")
+
+        val fn = intent.getStringExtra("firstName")
         if (fn != null) {
             findViewById<TextView>(R.id.birthDateText).setText("Set your birthdate, $fn").toString()
         }
-        findViewById<CalendarView>(R.id.birthDatePicker)
-        findViewById<Button>(R.id.submitBdateBtn).setOnClickListener { submitBirthdate() }
-    }
-    private fun submitBirthdate(){
 
-        goToUserProfile()
+        val picker: DatePicker = findViewById<DatePicker>(R.id.birthDatePicker) as DatePicker
+        var birthdate = "${picker.month+1}/${picker.dayOfMonth}/${picker.year}"
+        picker.init(1999, 8, 1, object : DatePicker.OnDateChangedListener {
+            override fun onDateChanged(view: DatePicker?, year: Int, monthOfYear: Int, dayOfMonth: Int) {
+                birthdate = "${picker.month+1}/${picker.dayOfMonth}/${picker.year}"
+            }
+        })
 
-    }
-    private fun goToUserProfile(){
-        val intent = Intent(this, UserProfile::class.java)
-        intent.putExtra("firstName", intent.getStringExtra("firstName"));
-        intent.putExtra("lastName", intent.getStringExtra("lastName"));
-        intent.putExtra("emailAdd", intent.getStringExtra("emailAdd"));
-        intent.putExtra("age", intent.getStringExtra("age"));
-        intent.putExtra("phoneNum", intent.getStringExtra("phoneNum"));
-        //intent.putExtra("Birthdate", birth)
-        startActivity(intent)
+        submitBdateBtn.setOnClickListener {
+            var birth = birthdate.toString()
+            val intent2 = Intent(this@NextActivity, UserProfile::class.java)
+            intent2.putExtra("firstName", intent.getStringExtra("firstName"))
+            intent2.putExtra("lastName", intent.getStringExtra("lastName"))
+            intent2.putExtra("emailAdd", intent.getStringExtra("emailAdd"))
+            intent2.putExtra("age", intent.getStringExtra("age"))
+            intent2.putExtra("phoneNum", intent.getStringExtra("phoneNum"))
+            intent2.putExtra("birthDate", birth)
+            startActivity(intent2)
+        }
     }
 }
